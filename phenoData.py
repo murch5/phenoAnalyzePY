c53056ebcfe697b3ce0bdfd175ee6e6c885f050a
murch <ryan.c.murchie@gmail.com>
2017-01-20 01:04:19 -0500

    added sys.path to plotmanager to load (temporary..) add plot manager object with sample violin plots

--- phenoData.py
+++ phenoData.py
@@ -1,10 +1,21 @@
+PATH = '/Users/Ryan/PycharmProjects/plot_managerPY'
+import sys
+
+print(sys.path)
+if not PATH in sys.path:
+    sys.path.append(PATH)
+
+
+
+
 #Phenotype data loading and manipulation class object
 
 import pandas as pd
 import glob as glob
 import os as os
 
-
+from plot_manager import plot_manager
+from violin import violin
 
 def loadCSVtoPandas(fileName):
     dataFrame = pd.read_csv(fileName, sep=",")
@@ -50,6 +61,12 @@ class phenoData():
         return slicedData
 
 f = phenoData("test/")
+
+c = plot_manager()
+
+c.addPlot(violin,((2,2,3,2,4,6,2),(1,2,2,2,1,1,1)),211)
+c.addPlot(violin,((1,2,2,2,1,1,1),(2,2,3,2,4,6,2)),212)
+c.drawPlots()
 
 f.extractData(0,0,["MuiseLabID","FamilyStatus"],0)
 print(f)
