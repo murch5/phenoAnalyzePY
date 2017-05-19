3363ebd3a4b061d6b0988abbb744a7680a7bb38e
murch <ryan.c.murchie@gmail.com>
2017-05-19 02:19:05 -0400

    add setup.py for install

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='plotanalyze',
+    version='0.1.0',
+    packages=['plotanalyze', 'plotanalyze.datatype'],
+    url='',
+    license='',
+    author='Ryan',
+    author_email='',
+    description='Data analysis and plotting suite'
+)
