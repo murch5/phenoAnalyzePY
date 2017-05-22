88d97e1a6373c2edfc273a283944230811063ff0
murch <ryan.c.murchie@gmail.com>
2017-05-22 00:01:28 -0400

    massive updates to plot_analyze redesigned func flow added logging

--- plotanalyze/plot_analyze.py
+++ plotanalyze/plot_analyze.py
@@ -6,6 +6,11 @@ import glob as glob
 import os as os
 import sys as sys
 import pandas as pd
+import numpy as np
+
+import xml.etree.ElementTree as et
+import datetime as datetime
+import copy as copy
 
 #Import plot manager
 import plotmanager.plot_manager as pltmanager
@@ -16,6 +21,8 @@ from plotanalyze import processingFunc as process, generateFrameSet as gfs
 from plotanalyze.datatype.GFFdata import GFF
 
 import datatypes as dt
+
+import plotanalyze.view as view
 
 #Append the plotanalyze path to sys for cmd line
 sys.path.append(os.path.dirname(__file__))
@@ -28,375 +35,156 @@ data_types = {}
 data_types.update(dict.fromkeys(["csv","tsv"],dt.table.Table))
 data_types.update(dict.fromkeys(["tif","png"],dt.image.Image))
 
+#Dictionary containing keys for various log message types
+msg_type = {}
+msg_type.update(dict.fromkeys(["HEAD","H",0],"HEAD"))
+msg_type.update(dict.fromkeys(["INFO","I",1],"INFO"))
+msg_type.update(dict.fromkeys(["WARN","W",2],"WARN"))
+msg_type.update(dict.fromkeys(["ERRO","E",3],"ERRO"))
+
+def log(msg,indent,type="INFO"):
+    ind = ""
+    for steps in range(1,indent):
+        ind = ind + "\t"
+    print(msg_type[type] + "\t" + str(datetime.datetime.now()) + "\t" + ind + msg)
 
 class PlotAnalyze:
 
     def deploy_data(self, input="./input/"):
         #Deploy data function
-        print("INFO     Deploying data...")
         #Get the name of all directories in current working directory
         #Check if input directory already exists
 
         if os.path.isfile(input):
             extension = os.path.splitext(input)[1]
             if extension == ".txt" or extension == "":
-                print("INFO         Input file list detected")
+                log("Input file list detected",2,1)
             elif extension == ".gz" or extension == ".tgz":
-                print("INFO         Input gzip tar ball detected")
+                log("Input gzip tar ball detected",1,1)
                 return
         elif os.path.exists(input):
-            print("INFO         Input path detected")
-            print("INFO         Compiling data file list...")
+            log("Input path detected",2,1)
+            log("Compiling data file list...",3,1)
             self.data_filelist = glob.glob(input + "**/*.*",recursive=True)
-            print(self.data_filelist)
+
 
         for file in self.data_filelist:
-            ext = os.path.splitext(file)[1][1:]
-            data_temp = data_types[ext]()
+            filename, ext = os.path.splitext(os.path.basename(file))
+            data_temp = data_types[ext[1:]]()
             data_temp.set_filename(file)
+            data_temp.set_name(filename)
             self.data.append(data_temp)
 
-            #self.data.append(
+        return
 
-            print(ext)
+    def load_viewset(self, viewset):
+
+        if os.path.isfile(viewset):
+                log("View set file detected",2,1)
+                self.viewset_filelist = viewset
+                self.viewset_xml = et.parse(viewset)
+
+        elif os.path.exists(viewset):
+            log("View set directory detected",2,1)
+            log("Compiling view sets...",3,1)
+            self.viewset_filelist = glob.glob(viewset + "**/*.*")
+
 
         return
 
-    def load_viewset(self):
-        return
+
     def process_viewset(self):
+
+        view_root = self.viewset_xml.getroot()
+
+        for viewset in view_root.iter("viewset"):
+            new_viewset = view.Viewset()
+            new_viewset.set_title(viewset.findtext("title"))
+            new_viewset.set_xml(viewset)
+
+            for views in viewset.iter("view"):
+                new_view = view.View()
+                new_view.set_title(views.findtext("title"))
+                new_view.set_xml(views)
+                new_view.set_viewset_style_XML(viewset)
+                new_view.init_plot()
+
+                new_viewset.views.append(new_view)
+
+            self.viewset.append(new_viewset)
+
         return
+
     def load_data(self):
+
+        #Determine which data is active by which datasets are referenced in viewset.xml
+        active_datasets = self.viewset_xml.findall(".//dataset/name")
+
+        for active in active_datasets:
+            self.data_active_list.append(active.text)
+
+        self.data_active_list = set(self.data_active_list)
+
+        for data in self.data:
+            if data.get_name() in self.data_active_list:
+                data.load()
+                self.data_active.append(data)
+
+        del self.data[:]
+
         return
+
     def process_data(self):
+
+        index_dict = dict(zip(self.data_active_list,range(0,len(self.data_active_list))))
+
+        for viewset in self.viewset:
+            for view in viewset.views:
+                plot_set = view.get_xml().findall(".//plot")
+                for plot in plot_set:
+                    subplots_set = plot.findall(".//subplot")
+                    for subplot in subplots_set:
+                        datasets = subplot.findall(".//data//dataset")
+                        for data in datasets:
+                            data_name = data.findtext("name")
+                            index = index_dict[data_name]
+                            new_data = copy.deepcopy(self.data_active[index])
+                            new_data.processing(data.find(".//processing"))
+
+                        view.figure.add_plot(new_data,subplot.findtext("plottype"),subplot)
+
         return
+
     def show(self):
+
+        for viewset in self.viewset:
+            viewset.show_views()
+
         return
 
     def cleanup(self):
         return
-    def getFilesFromDir(self, directory):
-
-        fileName = []
-        self.dataDir = glob.glob("./input/*")
-
-        for y in self.dataDir:
-            dirFiles = glob.glob(os.path.join(y, "*"))
-            self.dataDirNames.append(y)
-            fileName.append(dirFiles)
-
-        return fileName
-
-    def getViewsFromDir(self, directory):
-
-
-        dirFiles = glob.glob(os.path.join(directory, "*.viewSet"))
-
-        return dirFiles
 
     def __init__(self, directory):
         self.directory = directory  # directory containing the phenoData to load
-        self.dataDir = []
-        self.dataDirNames = []
-        self.fileNames = self.getFilesFromDir(self.directory)
-        self.data = self.loadData()
-        self.dfNames = self.getNames()
-        self.figureArgList = []
 
         self.data_filelist = None
-
-        # View specific variables
-        self.viewSetRaw = []
-        self.viewSet = []
-        self.plotManagers = []
-        self.viewNames = []
-
-        self.currView = 0
-        self.totalView = 0
+        self.viewset_filelist = None
+        self.viewset_xml = None
+        self.data = []
+        self.viewset = []
+        self.data_active_list = []
+        self.data_active = []
 
         return
-
-    def getNames(self):
-
-        names = []
-
-        for q in self.fileNames:
-            for x in q:
-                r = os.path.basename(x)
-                t = os.path.splitext(r)
-
-                names.append(t[0])
-
-        return names
-
-    def getDFIndexFromName(self, name):
-
-        index = self.dfNames.index(name)
-        return index
-
-    def loadViewSets(self, dir):
-
-        viewFile = self.getViewsFromDir(dir)
-
-        for i in viewFile:
-            with open(i, 'r') as csvfile:
-                viewSetFile = csv.reader(csvfile, delimiter=",")
-                for row in viewSetFile:
-                    self.viewSetRaw.append(row)
-
-        return
-
-    def arrayCheck(self, entry):
-        if ";" in entry:
-            entry = entry.split(";")
-
-        return entry
-
-    def parseRawViewData(self):
-
-        viewIndices = []
-        line = -1
-
-        for q in self.viewSetRaw:
-            line += 1
-            if q[0][0] == ">":
-                viewIndices.append(line)
-
-        numViews = len(viewIndices)
-
-        viewIndices.append(len(self.viewSetRaw))
-        figureArgList = []
-
-        for i in range(0, numViews):
-            figureParam = self.viewSetRaw[viewIndices[i]][0][1:]
-            figureParam = figureParam.split(",")
-            self.viewNames.append(figureParam[0])
-            self.figureArgList.append(figureParam[1:])
-
-            entryList = []
-            for q in range(viewIndices[i] + 1, (viewIndices[i + 1])):
-                temp = [self.arrayCheck(self.viewSetRaw[q][0]), self.arrayCheck(self.viewSetRaw[q][1]),
-                        self.arrayCheck(self.viewSetRaw[q][2]), self.arrayCheck(self.viewSetRaw[q][3]),
-                        self.arrayCheck(self.viewSetRaw[q][4]), self.arrayCheck(self.viewSetRaw[q][5]),
-                        self.arrayCheck(self.viewSetRaw[q][6]), self.arrayCheck(self.viewSetRaw[q][7]),
-                        self.arrayCheck(self.viewSetRaw[q][8])]
-                entry = pd.Series(temp, name=self.viewSetRaw[q][0],
-                                  index=["title", "position", "plotType", "dataSet", "data", "func", "dataArgs",
-                                         "plotArgs", "annotate"])
-                entryList.append(entry)
-
-            view = pd.DataFrame(entryList)
-            self.viewSet.append(view)
-
-        return
-
-    def parseView(self):
-
-        viewIndex = -1
-
-        for x in self.viewSet:
-
-            viewIndex += 1
-            newplot = pltmanager.plot_manager(self.viewNames[viewIndex], self.figureArgList[viewIndex])
-
-            for index, row in x.iterrows():
-                dataTemp = self.extractData(row[3], row[4], row[6])
-
-                processTemp = self.processData(dataTemp, row[5])
-
-                newplot.addPlot(row[0], row[1], pltmanager.chartTypes[(row[2])], processTemp, row[7], row[8])
-
-            self.plotManagers.append(newplot)
-
-        return
-
-    def setView(self, view):
-        self.currView = view
-        return
-
-    def displayAllViews(self):
-
-        for plots in self.plotManagers:
-            plots.drawPlots()
-
-        self.plotManagers[0].showPlot()
-
-        return
-
-    def loadCSV(self, fileName):
-
-        dataFrame = pd.read_csv(fileName, sep=",")
-
-        return dataFrame
-
-    def loadTIF(self, fileName):
-
-        data = gfs.loadTIF(fileName)
-
-        return data
-
-    def loadDir(self, directory):
-
-        files = glob.glob(os.path.join(directory, "*.*"))
-
-        data = gfs.generateFrameSet(files)
-
-        return data
-
-    def loadGFF(self, fileName):
-
-        data = GFF(fileName)
-
-        return data
-
-    fileTypes = {"csv": loadCSV, "tif": loadTIF, "": loadDir, "gff": loadGFF}
-
-    def loadData(self):
-
-        dataSet = []
-        for i in self.fileNames:
-            for q in i:
-                extension = os.path.splitext(q)[1][1:]
-                data = PlotAnalyze.fileTypes[extension](self, q)
-                dataSet.append(data)
-
-        return dataSet
-
-    sortType = {"ascend": True, "descend": False}
-
-    def sort(self, data, arg):
-
-        newdata = pd.DataFrame(data)
-        direction = arg[0]
-        newdata.sort_values(by=0, ascending=PlotAnalyze.sortType[direction], inplace=True)
-        return newdata
-
-    def sortIndex(self, data, arg):
-
-        newdata = pd.DataFrame(data)
-        direction = arg[0]
-        newdata.sort_index(ascending=PlotAnalyze.sortType[direction], inplace=True)
-        return newdata
-
-    def filterByVal(self, data, arg):
-
-        newdata = pd.DataFrame(data)
-
-        for x in arg:
-            newdata = newdata.query(x)
-
-        newdata.reset_index(drop=True, inplace=True)
-
-        return newdata
-
-    def filterNA(self, data, arg):
-
-        newdata = pd.DataFrame(data)
-
-        newdata = newdata.dropna(0, subset=[arg])
-
-        return newdata
-
-    def groupby(self, data, arg):
-
-        newdata = data[:]
-
-        newdata = newdata.groupby(by=arg).size()
-
-        return newdata
-
-    def groupbyval(self, data, arg):
-
-        if isinstance(data, pd.DataFrame):
-
-            newdata = data.ix[:, 0]
-
-        else:
-            newdata = data[:]
-
-        if "inclNA" in arg:
-            newdata.fillna("n/a", inplace=True)
-
-        if "Delim" in arg:
-            newDatasplit = newdata.str.split(";").apply(pd.Series, 1).stack()
-
-            newdata = newDatasplit
-
-        newdata = newdata.groupby(newdata).size()
-        newdata.columns = ["Index", "GroupedCounts"]
-
-        return newdata
-
-    def filterByID(self):
-        return
-
-    dataFunc = {"filterByVal": filterByVal, "groupBy": groupby, "groupByVal": groupbyval, "sort": sort,
-                "sortIndex": sortIndex, "filterNA": filterNA}
-    processFunc = {"gaussian": process.gaussian, "variance": process.variance, "stddev": process.stddev}
-
-    def parseArgs(self, args):
-        argList = []
-        if args.any():
-            for i in args:
-                q = i.split("=", 1)
-                argList.append(q)
-
-        return argList
-
-    def evaluateArg(self, data, arg):
-        temp = data
-        argList = arg[1].split("~")
-        temp = PlotAnalyze.dataFunc[arg[0]](self, temp, argList)
-
-        return temp
-
-    def evaluateProcess(self, data, arg):
-        temp = data
-        argList = arg[1].split("~")
-        temp = PlotAnalyze.processFunc[arg[0]](temp, argList)
-
-        return temp
-
-    def extractData(self, dataSet, subset, args):
-
-        pdArg = pd.Series(args)
-        f = self.parseArgs(pdArg)
-
-        # slicedData = self.data[int(dataSet)]
-
-        slicedData = self.data[self.getDFIndexFromName(dataSet)]
-
-        if (subset != "FULL"):
-            df = slicedData[subset]
-        else:
-            df = slicedData
-
-        for x in f:
-            if x != ["0"]:
-                df = self.evaluateArg(df, x)
-
-        return df
-
-    def processData(self, dataSet, args):
-
-        tempData = dataSet
-        pdArg = pd.Series(args)
-        argList = self.parseArgs(pdArg)
-
-        for x in argList:
-            if x != ["0"]:
-                tempData = self.evaluateProcess(tempData, x)
-
-        return tempData
 
 
 def main(argv):
 
     directory = "./"
-    viewSetDir = "./views"
+    viewset = "./views/venn2.xml"
     input = "./input/"
-
-    output = 0
+    output = "./output/"
 
     try:
         opts, args = getopt.getopt(argv,"hi:o:",["dir=","view="])
@@ -412,23 +200,30 @@ def main(argv):
         elif opt in ("-o", "--output"):
             output = arg
         elif opt in ("-v", "--view"):
-            viewSetDir = arg
+            viewset = arg
 
-
-
-    print("INFO     Plot Analyze v" + VERSION)
+    log("Plot Analyze v" + VERSION,0,0)
+    log("**********************************", 0, 0)
 
     plotanalyze = PlotAnalyze(directory)
 
+    log("Deploying data...", 1, 1)
     plotanalyze.deploy_data(input)
 
-    plotanalyze.loadViewSets(viewSetDir)
+    log("Loading view set(s)...", 1, 1)
+    plotanalyze.load_viewset(viewset)
 
-    plotanalyze.parseRawViewData()
-    plotanalyze.parseView()
-    plotanalyze.setView(0)
-    plotanalyze.displayAllViews()
+    log("Processing view set(s)...", 1, 1)
+    plotanalyze.process_viewset()
 
+    log("Loading data...", 1, 1)
+    plotanalyze.load_data()
+
+    log("Processing data...", 1, 1)
+    plotanalyze.process_data()
+
+    log("Show plots...", 1, 1)
+    plotanalyze.show()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
